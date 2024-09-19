b52d09ababf842b4fa96d382f4cdf3db29b8bf0f
Verin Pavel <verinpavel4@gmail.com>
2024-09-20 00:12:08 +0300

    fix puth to back img banner form

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       additionalData: `@import "./src/index";`, // Импорт общего файла SCSS
-  //     },
-  //   },
-  // },
+  css: {
+    preprocessorOptions: {
+      scss: {
+      },
+    },
+  },
+  
 })
 
